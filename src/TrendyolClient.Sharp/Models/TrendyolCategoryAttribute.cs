d6016c0442911cff64ef93dc25e62c393f106d36
Berkay <77205615+bberka@users.noreply.github.com>
2025-12-03 20:26:46 +0300

    fix: add missing properties to TrendyolCategoryAttribute

--- src/TrendyolClient.Sharp/Models/TrendyolCategoryAttribute.cs
+++ src/TrendyolClient.Sharp/Models/TrendyolCategoryAttribute.cs
@@ -17,6 +17,21 @@ public class TrendyolCategoryAttribute
     [JsonPropertyName("allowCustom")]
     public bool AllowCustom { get; set; }
 
+    [JsonPropertyName("displayName")]
+    public string DisplayName { get; set; }
+
+    [JsonPropertyName("attributeId")]
+    public long? AttributeId { get; set; }
+
+    [JsonPropertyName("attributeName")]
+    public string AttributeName { get; set; }
+
+    [JsonPropertyName("slicer")]
+    public bool? Slicer { get; set; }
+
+    [JsonPropertyName("varianter")]
+    public bool? Varianter { get; set; }
+
     [JsonPropertyName("attributeValues")]
     public List<TrendyolAttributeValue> AttributeValues { get; set; }
 }
